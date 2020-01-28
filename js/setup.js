72ff27ab92245da73b1b5bcd7eea370cbb97dd5a
Рябов <dmitryabov2017@yandex.ru>
2020-01-28 19:26:41 +0300

    поправил согласно замечаниям

--- js/setup.js
+++ js/setup.js
@@ -19,44 +19,44 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template').
 content.querySelector('.setup-similar-item');
 
 
-var getRandomInt = function (arr) {
-  return Math.floor(Math.random() * arr.length);
+var getRandomInt = function (array) {
+  return Math.floor(Math.random() * array.length);
 };
 
 
-var getWizards = function (numberOfWizards) {
+var generateWizards = function (numberOfWizards) {
   var wizards = [];
   for (var i = 0; i < numberOfWizards; i++) {
-    var object = {
+    var wizard = {
       name: WIZARD_NAMES[getRandomInt(WIZARD_NAMES)] + ' ' + WIZARD_SURNAMES[getRandomInt(WIZARD_SURNAMES)],
       coatColor: COAT_COLORS[getRandomInt(COAT_COLORS)],
       eyesColor: AYES_COLORS[getRandomInt(AYES_COLORS)]
     };
-    wizards.push(object);
+    wizards.push(wizard);
   }
   return wizards;
 };
 
 
-var renderWizards = function (arr) {
+var creatureWizard = function (array) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = arr.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = arr.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = arr.eyesColor;
+  wizardElement.querySelector('.setup-similar-label').textContent = array.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = array.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = array.eyesColor;
   return wizardElement;
 };
 
 
-var addWizardsToFragment = function (int) {
+var creatureFragmentWithWizards = function (int) {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < getWizards(int).length; i++) {
-    fragment.appendChild(renderWizards(getWizards(int)[i]));
+  for (var i = 0; i < generateWizards(int).length; i++) {
+    fragment.appendChild(creatureWizard(generateWizards(int)[i]));
   }
   return fragment;
 };
 
 
-similarListElement.appendChild(addWizardsToFragment(WIZARDS_NUMBER));
+similarListElement.appendChild(creatureFragmentWithWizards(WIZARDS_NUMBER));
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
 

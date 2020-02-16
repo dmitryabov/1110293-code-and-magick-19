3183fe87140a0e1df40d4209f4df7082c8c798cc
Рябов <dmitryabov2017@yandex.ru>
2020-02-16 18:50:19 +0300

    Учебный проект: pimp my mage

--- js/dialogColnrol.js
+++ js/dialogColnrol.js
@@ -1,10 +1,13 @@
 'use strict';
 
 (function () {
+  var ESC_KEY = 'Escape';
+  var DIALOG_OFFSET_LEFT = window.innerWidth / 2;
+  var DIALOG_OFFSET_TOP = 80;
   var userDialoge = document.querySelector('.setup');
   var inputUdserName = document.querySelector('.setup-user-name');
   var onPopupEscPress = function (evt) {
-    if (evt.key === window.const.ESC_KEY && inputUdserName !== document.activeElement) {
+    if (evt.key === ESC_KEY && inputUdserName !== document.activeElement) {
       window.dialogControl.closePopup();
     }
   };
@@ -22,8 +25,8 @@
     userDialoge.classList.add('hidden');
     document.addEventListener('keydown', onPopupEscPress);
     document.removeEventListener('keydown', onPopupEscPress);
-    setupDialogElement.style.top = window.const.DIALOG_OFFSET_TOP + 'px';
-    setupDialogElement.style.left = window.const.DIALOG_OFFSET_LEFT + 'px';
+    setupDialogElement.style.top = DIALOG_OFFSET_TOP + 'px';
+    setupDialogElement.style.left = DIALOG_OFFSET_LEFT + 'px';
   };
 
 
